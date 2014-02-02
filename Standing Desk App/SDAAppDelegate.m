16325fb15025773f0daf0a87a3244359d17b2db2
Michael Chadwick <michael.chadwick@gmail.com>
2014-02-02 11:10:39 -0800

    removed notification customImage as it was unneeded

--- Standing Desk App/SDAAppDelegate.m
+++ Standing Desk App/SDAAppDelegate.m
@@ -465,7 +465,6 @@ NSSound *standSound;
   // NSUserNotification won't play custom sounds in bundle and/or not in system sounds directories,
   // so we play a sound after the notification is displayed, getting around that
   //alert.soundName = soundFile;
-  alert.contentImage = [NSImage imageNamed:iconName];
   [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:alert];
   [self playSounds];
 }
